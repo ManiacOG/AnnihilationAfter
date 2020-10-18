7634825b68c16841026eb2d5f934e1f9569426af
ManiacOG <Steve@LAPTOP-0I7KAF5M>
2020-10-17 22:12:06 -0700

    Changed Icon

--- src/main/java/net/mcreator/annimath/itemgroup/AftermathItemGroup.java
+++ src/main/java/net/mcreator/annimath/itemgroup/AftermathItemGroup.java
@@ -6,8 +6,8 @@ import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.block.Blocks;
 
+import net.mcreator.annimath.item.CorruptedCrystalItem;
 import net.mcreator.annimath.AnnimathModElements;
 
 @AnnimathModElements.ModElement.Tag
@@ -22,7 +22,7 @@ public class AftermathItemGroup extends AnnimathModElements.ModElement {
 			@OnlyIn(Dist.CLIENT)
 			@Override
 			public ItemStack createIcon() {
-				return new ItemStack(Blocks.GRASS_BLOCK, (int) (1));
+				return new ItemStack(CorruptedCrystalItem.block, (int) (1));
 			}
 
 			@OnlyIn(Dist.CLIENT)
